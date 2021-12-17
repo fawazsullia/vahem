18efd3838080f00c47468cf1f979bb2fc76eef27
praguru14 <48213609+praguru14@users.noreply.github.com>
2021-12-17 08:41:48 +0530

    Added: Custom HTTP error functionality

---
+++ errorHttp.js
@@ -0,0 +1,10 @@
+// to throw a error use --> throw new ErrorHttp(errorCode,errorMessage);
+
+class ErrorHttp extends Error {
+    constructor(errorCode, message) {
+        super(message);
+        this.code = errorCode;
+    }
+}
+
+module.exports = ErrorHttp;
--- index.js
+++ index.js
@@ -1,12 +1,13 @@
 const valid = require('./valid')
 const date = require('./date')
 const array = require('./array')
-
+const ErrorHttp = require('./ErrorHttp')
 
 
 
 module.exports = {
     valid,
     date,
-    array
+    array,
+    ErrorHttp
 }
